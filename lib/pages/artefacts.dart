4a8c6314435fb32d1b78ad0f88b59c73468648ed
SpatialKnots <univerekb@mail.ru>
2021-10-31 10:05:20 +0500

    page skeleton

---
+++ lib/pages/artefacts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArtefactScreen extends StatelessWidget {
+  const ArtefactScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
